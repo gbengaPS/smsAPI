61f98524c8312f9785813c7c0eb34dd427cc4ddf
gbengaPS <ioyetade@gmail.com>
2018-11-07 23:26:07 +0100

    feat(get-messages): create controller and middleware

--- server/controllers/messages/index.js
+++ server/controllers/messages/index.js
@@ -1,4 +1,5 @@
 const { messages } = require('../../models');
+const { sendInternalServerError } = require('../../lib/utils');
 
 class MessageController {
   static sendMessage(req, res) {
@@ -14,6 +15,38 @@ class MessageController {
       })
       .catch(() => {
         res.status(500).send({ error: 'Your request made our server behave in an unusual way' });
+      });
+  }
+
+  static getSentMessages(req, res) {
+    const { user } = req;
+    messages
+      .findAll({ where: { sender: user.phoneNumber } })
+      .then((userMessages) => {
+        if (!userMessages.length) {
+          res.status(404).send({ message: 'User does not have any messages sent' });
+        } else {
+          res.status(200).send({ messages: userMessages });
+        }
+      })
+      .catch(() => {
+        sendInternalServerError(res);
+      });
+  }
+
+  static getReceivedMessages(req, res) {
+    const { user } = req;
+    messages
+      .findAll({ where: { receiver: user.phoneNumber } })
+      .then((userMessages) => {
+        if (!userMessages.length) {
+          res.status(404).send({ message: 'User has not received any messages' });
+        } else {
+          res.status(200).send({ messages: userMessages });
+        }
+      })
+      .catch(() => {
+        sendInternalServerError(res);
       });
   }
 }
